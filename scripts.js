e6898b554d6a617aa7eeebc4967b4a977f92d774
MixterioH <hende9canal@gmail.com>
2025-07-15 10:30:38 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const html = document.documentElement
+
+function toggleTheme() {
+	// if (html.classList.contains("light")) {
+	// 	html.classList.remove("light")
+	// } else {
+	// 	html.classList.add("light")
+	// }
+
+	html.classList.toggle("light")
+}
